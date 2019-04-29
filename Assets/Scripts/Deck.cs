5d200b6138fc10dad4c014e4cb2e05c88e63162b
Scott <scott_steffes@capsulatedsoftware.com>
2019-04-29 18:17:15 +0900

    deck fixes

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -8,84 +8,103 @@ public class Deck
 {
     [SerializeField]
     StartingDeck startingDeck;
-    List<CardData> deck;
-    
-    int _currentIndex;
-    int currentIndex{
-        get{
-            return _currentIndex % deck.Count;
-        }
-        set{
-            _currentIndex = value;
-            if(_currentIndex < 0)
-                _currentIndex = 0;
-        }
-    }
 
-    public void Initialize(){
-        deck = new List<CardData>(startingDeck.masterList.Length);
-        for (int i = 0; i < startingDeck.masterList.Length; i++)
-        {
-            deck.Add(startingDeck.masterList[i]);
-        }
-        // SortInPlaceRandom(deck);
-        currentIndex = 0;
-    }
+    CardList mainList = new CardList();
 
-    private void SortInPlaceRandom(List<CardData> cardList) {
-        for (int t = 0; t < cardList.Count; t++) {
-            CardData tmp = cardList[t];
-            int r = UnityEngine.Random.Range(t, cardList.Count);
-            cardList[t] = cardList[r];
-            cardList[r] = tmp;
-        }
-    }
-
-    public void Reset(){
-        currentIndex = 0;
+    public void Initialize() {
+        mainList.Initialize(startingDeck.masterList);
     }
 
     public Tuple<CardData, CardData> DrawCards(){
-        return new Tuple<CardData, CardData>(GetNextDrawable(), GetNextDrawable());
+        CardData firstCard = mainList.GetNextDrawable();
+        CardData secondCard;
+        do {
+            secondCard = mainList.GetNextDrawable();
+        } while (firstCard.title == secondCard.title);
+
+        return new Tuple<CardData, CardData>(firstCard, secondCard);
     }
 
-    CardData GetNextDrawable(){
-        CardData c = null;
-        while(c == null){
-            if(deck[currentIndex])
-                c = deck[currentIndex];
-            currentIndex++;
+    public void AddToDeck(CardData[] cards) {
+        mainList.AddToDeck(cards);
+    }
+
+    public void RemoveFromDeck(CardData card) {
+        mainList.RemoveFromDeck(card);
+    }
+
+    private class CardList {
+        List<CardData> theList = new List<CardData>();
+
+        public void Initialize(CardData[] startingDeck) {
+            theList = new List<CardData>(startingDeck.Length);
+            for (int i = 0; i < startingDeck.Length; i++) {
+                theList.Add(startingDeck[i]);
+            }
+            SortInPlaceRandom(theList);
+            currentIndex = 0;
         }
-        return c;
-    }
 
-    public void RemoveFromDeck(CardData card)
-    {
-        for (int i = 0; i < deck.Count; i++)
-        {
-            if(deck[i] == card){
-                deck.RemoveAt(i);
-                currentIndex--;
-                return;
+        int _currentIndex;
+        int currentIndex {
+            get {
+                return _currentIndex % theList.Count;
+            }
+            set {
+                _currentIndex = value;
+                if (_currentIndex < 0)
+                    _currentIndex = 0;
+                if(_currentIndex >= theList.Count) {
+                    _currentIndex = 0;
+                    SortInPlaceRandom(theList);
+                }
             }
         }
-        Debug.LogError("Card Remove failed");
-    }
 
-    public void AddToDeck(CardData[] cards){
-        if(cards == null)
-            return;
-        for (int i = 0; i < cards.Length; i++)
-        {
-            RegisterUsableCardAtRandomPosition(cards[i]);
+        private void SortInPlaceRandom(List<CardData> cardList) {
+            for (int t = 0; t < cardList.Count; t++) {
+                CardData tmp = cardList[t];
+                int r = UnityEngine.Random.Range(t, cardList.Count);
+                cardList[t] = cardList[r];
+                cardList[r] = tmp;
+            }
         }
-    }
 
-    void RegisterUsableCardAtRandomPosition(CardData card) {
-        int randomIndex = UnityEngine.Random.Range(0, deck.Count);
-        deck.Insert(randomIndex, card);
-        if(randomIndex < currentIndex){
-            currentIndex++;
+        public CardData GetNextDrawable() {
+            CardData c = null;
+            while (c == null) {
+                if (theList[currentIndex])
+                    c = theList[currentIndex];
+                currentIndex++;
+            }
+            return c;
+        }
+
+        public void RemoveFromDeck(CardData card) {
+            for (int i = 0; i < theList.Count; i++) {
+                if (theList[i] == card) {
+                    theList.RemoveAt(i);
+                    currentIndex--;
+                    return;
+                }
+            }
+            Debug.LogError("Card Remove failed");
+        }
+
+        public void AddToDeck(CardData[] cards) {
+            if (cards == null)
+                return;
+            for (int i = 0; i < cards.Length; i++) {
+                RegisterUsableCardAtRandomPosition(cards[i]);
+            }
+        }
+
+        void RegisterUsableCardAtRandomPosition(CardData card) {
+            int randomIndex = UnityEngine.Random.Range(0, theList.Count);
+            theList.Insert(randomIndex, card);
+            if (randomIndex < currentIndex) {
+                currentIndex++;
+            }
         }
     }
 }
